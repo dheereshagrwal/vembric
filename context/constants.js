9fd6f04ccd60b20a8f720c1fbe4efd78febdb4d9
Dheeresh Agarwal <da11@iitbbs.ac.in>
2025-06-14 08:09:39 +0530

    new commit

--- context/constants.js
+++
@@ -1,4 +0,0 @@
-import market from "./NFTMarketplace.json";
-
-export const MarketAddress = "0x09b55Df8eF2E83cd7DAC879F2149431a17D4BBee";
-export const MarketAddressABI = market.abi;
--- utils/shortenAddress.js
+++
@@ -1,3 +0,0 @@
-export const shortenAddress = (address) => {
-  return `${address.slice(0, 5)}...${address.slice(-4)}`;
-};
