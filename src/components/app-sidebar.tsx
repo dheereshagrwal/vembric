33bfa92d06b819884c8b5b5619d56313acb2eb0e
Chirag-kumar-dev <ck98738687@gmail.com>
2025-07-27 18:14:55 +0530

    sidebar navigation with UserName in footer

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,65 +1,110 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-
+import { ChevronUp, Plus, User2 } from "lucide-react"
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
+  SidebarGroupAction,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
 
-// Menu items.
-const items = [
+// Navigation items
+export const navigation = [
   {
-    title: "Home",
-    url: "#",
-    icon: Home,
+    title: "Guides",
+    links: [
+      { title: "Introduction", href: "/" },
+      { title: "Quickstart", href: "/quickstart" },
+      { title: "SDKs", href: "/sdks" },
+      { title: "Authentication", href: "/authentication" },
+      { title: "Pagination", href: "/pagination" },
+      { title: "Errors", href: "/errors" },
+      { title: "Webhooks", href: "/webhooks" },
+    ],
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
+    title: "Resources",
+    links: [
+      { title: "Contacts", href: "/contacts" },
+      { title: "Conversations", href: "/conversations" },
+      { title: "Messages", href: "/messages" },
+      { title: "Groups", href: "/groups" },
+      { title: "Attachments", href: "/attachments" },
+    ],
   },
 ]
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
+      <SidebarContent className="flex flex-col justify-between h-full">
+        <SidebarGroup className="h-full">
+          {/* <SidebarGroupLabel>Application</SidebarGroupLabel> */}
+          <SidebarGroupAction>
+            {/* <Plus /> <span className="sr-only">Add Project</span> */}
+          </SidebarGroupAction>
+          <SidebarGroupContent className="flex-grow">
             <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+              {navigation.map((group) => (
+                <Collapsible key={group.title} defaultOpen className="group/collapsible">
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton>
+                        <span>{group.title}</span>
+                      </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {group.links.map((link) => (
+                          <SidebarMenuSubItem key={link.title}>
+                            <SidebarMenuButton asChild>
+                              <a href={link.href}>
+                                <span>{link.title}</span>
+                              </a>
+                            </SidebarMenuButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
+          <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <User2 /> Chirag Kumar
+                    <ChevronUp className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-popper-anchor-width] bg-white"
+                >
+                  <DropdownMenuItem>
+                    <span>Sign out</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
