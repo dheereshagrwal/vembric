fecc0612ec876eca8e3ff047699ab5e6b10c8884
Chirag-kumar-dev <ck98738687@gmail.com>
2025-08-11 00:51:30 +0530

    home page added on click of logo

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -22,15 +22,17 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar";
 import { sidebarData } from "@/lib/sidebar-data";
+import { useRouter } from "next/navigation";
 
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const router = useRouter();
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <div className="flex items-center gap-2 px-4 py-2">
           <img src="/logo.svg" alt="Vembric Logo" className="size-8" />
-          <span className="truncate font-semibold">Vembric</span>
+          <span className="truncate font-semibold cursor-pointer" onClick={()=>{router.replace("/")}}>Vembric</span>
         </div>
       </SidebarHeader>
       <SidebarContent>
