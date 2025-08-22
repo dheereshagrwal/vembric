e469607aade4ac178ff566f68b7e322d79a832ab
Dheeresh Agarwal <da11@iitbbs.ac.in>
2025-08-22 17:41:00 +0530

    new pnpm

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -11,13 +11,20 @@ import {
 } from "@/components/ui/sidebar";
 import { sidebarData } from "@/constants/";
 import Link from "next/link";
+import Image from "next/image";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <Link href="/" className="flex items-center gap-2 px-4 py-2">
-          <img src="/logo.svg" alt="Vembric Logo" className="size-8" />
+          <Image
+            src="/logo.svg"
+            alt="Vembric Logo"
+            width={32}
+            height={32}
+            className="size-8"
+          />
           <span className="truncate font-semibold">Vembric</span>
         </Link>
       </SidebarHeader>
